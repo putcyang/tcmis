d79cd64b14cd64bf1bded916f825423281af702d
tcyangpu <tcyangpu@gmail.com>
2023-09-02 11:39:57 +0800

    make it better

--- index.py
+++ index.py
@@ -110,7 +110,21 @@ def search():
 
 @app.route("/searchQ", methods=["POST","GET"])
 def searchQ():
-    return render_template("input.html")
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        info = ""     
+        collection_ref = db.collection("電影")
+        #docs = collection_ref.where("title","==", "夜鷹的單戀").get()
+        docs = collection_ref.order_by("showDate").get()
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]: 
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+      info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
 
 #if __name__ == "__main__":
     #app.run()

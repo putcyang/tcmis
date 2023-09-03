676c02db8229ff61fe56d0e6152eab55b060ce91
tcyangpu <tcyangpu@gmail.com>
2023-09-03 12:35:08 +0800

    make it better

--- index.py
+++ index.py
@@ -136,22 +136,9 @@ def webhook():
     #info = "動作：" + action + "； 查詢內容：" + msg
     if (action == "rateChoice"):
         rate =  req.get("queryResult").get("parameters").get("rate")
-        if (rate == "輔12級"):
-            rate = "輔導級(未滿十二歲之兒童不得觀賞)"
-        elif (rate == "輔15級"):
-            rate = "輔導級(未滿十五歲之人不得觀賞)"
-        info = "您選擇的電影分級是：" + rate + "，相關電影：\n"
-
-        collection_ref = db.collection("電影")
-        docs = collection_ref.get()
-        result = ""
-        for doc in docs:
-            dict = doc.to_dict()
-            if rate in dict["rate"]:
-                result += "片名：" + dict["title"] + "\n"
-                result += "介紹：" + dict["hyperlink"] + "\n\n"
-        info += result
+        info = "您選擇的電影分級是：" + rate
     return make_response(jsonify({"fulfillmentText": info}))
+
 
 
 #if __name__ == "__main__":

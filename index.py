61558ed0bc74e533a716d44853c211d133d68875
tcyangpu <tcyangpu@gmail.com>
2023-09-03 12:29:29 +0800

    make it better

--- index.py
+++ index.py
@@ -127,7 +127,7 @@ def searchQ():
         return render_template("input.html")
 
 @app.route("/webhook", methods=["POST"])
-def webhook3():
+def webhook():
     # build a request object
     req = request.get_json(force=True)
     # fetch queryResult from json
@@ -142,10 +142,16 @@ def webhook3():
         elif (rate == "輔15級"):
             rate = "輔導級(未滿十五歲之人不得觀賞)"
         info = "您選擇的電影分級是：" + rate + "，相關電影：\n"
-
+        collection_ref = db.collection("電影")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
     return make_response(jsonify({"fulfillmentText": info}))
-
-
 
 #if __name__ == "__main__":
     #app.run()

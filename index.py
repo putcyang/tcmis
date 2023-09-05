d7d0a0cca09db5a8283fb6fdfce0370e81666b18
tcyangpu <tcyangpu@gmail.com>
2023-09-05 11:27:36 +0800

    make it better

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import openai
 import os
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY1")
 
 
 import requests, json
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊子青Python網頁b</h1>"
+    homepage = "<h1>楊子青Python網頁c</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
